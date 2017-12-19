6af80a1e72768a733738291ddc3cef3dc8fcbb35
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2017-12-19 14:07:55 +0700

    Get Started Baru Nampilin

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './screens/Home'
+
+export default class App extends React.Component {
+  constructor () {
+    super ()
+  }
+  render() {
+    return (
+      <Home/>
+    );
+  }
+}
---
+++ client/screens/Details.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class Details extends React.Component({
+  static navigationOptions = (navigation, screensProps) => ({
+    title: 'DETAILS PAGES'    
+  })
+})

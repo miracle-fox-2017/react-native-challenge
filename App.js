a725b85717ce5ba4c9b025b70e95561891e6fed9
setiadijoe <dennymiloen@gmail.com>
2017-12-20 13:39:26 +0700

    get api

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
 
+const http = axios.create({
+  baseURL: 'https://swapi.co/api'
+})
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      people: []
+    }
+  }
+
+  componentWillMount ( {
+    http.get('/people')
+      .then(({data})=> {
+        console.log('data yang masuk ', data.results);
+        this.setState({
+          people: data.results
+        })
+      })
+  })
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{JSON.stringify(this.state.people)}</Text>
       </View>
     );
   }
